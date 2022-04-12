76521c1580731f3e1f6eaf0173382b0e93c07fde
OK_Diamond <ok.thekirks@gmail.com>
2022-04-12 20:42:34 +0100

    Reaction Time Game now fully integrated with MySQL. Also added class and function descriptions to make it feel more polished.

--- reaction_time_game/db_code/__init__.py
+++ reaction_time_game/db_code/__init__.py
@@ -1,19 +1,20 @@
 class db_class:
-    '''Takes conn and table_name as parameters on initialisation
-    'conn' is the sql database connection'''
-    def __init__(self, conn="", table_name=""):
+    def __init__(self, conn, table_name:str):
+        '''Takes conn and table_name as parameters on initialisation
+        'conn' is the sql database connection'''
         self.conn = conn
-        #self.cursor = conn.cursor()
+        self.cursor = conn.cursor()
         self.table_name = table_name
 
-    def create_table(self, *columns:str):
+    def create_table(self, *columns:str) -> None:
         '''Creates a table using the table name specified on initialisation. Columns should be given in SQL format, for example:
         "userName TEXT", "time FLOAT", "PRIMARY KEY (userName)"'''
         sql_inp = f"CREATE TABLE {self.table_name} ("
         for i in columns: sql_inp += i+", "
         sql_inp = sql_inp[:-2]+")"
-        try: self.cursor.execute(sql_inp)
-        except: pass
+        try: 
+            self.cursor.execute(sql_inp)
+        except: print("create_table error")
 
     def add_rec(self, *args) -> None:
         '''Adds a record to the database specified with table_name. Each arg is a column of the table.'''
@@ -24,7 +25,7 @@ class db_class:
         try:
             self.cursor.execute(sql_inp, (args))
             self.conn.commit()
-        except: pass
+        except: print("add_rec error")
 
     def update_rec(self, set_name, set_val, key_name, key_val) -> None:
         '''Update a record. 'set' is the record to be updated and 'key' is the primary key'''
@@ -34,32 +35,26 @@ class db_class:
             self.conn.commit()
         except: pass
 
-    def get_rec(self, fetchone=True, columns="*", **conditions) -> list:
-        '''get_rec(self, fetchone=True, column="*", **conditions)
-
-        If fetchall is False, fetchone will be used instead
-        Conditions are applied in the following ways (in listed order):
+    def get_rec(self, columns="*", **conditions) -> list:
+        '''Conditions are applied in the following ways (in listed order):
             where : appends " WHERE "+str(conditions["where"])
             order: appends " ORDER BY "+str(conditions["order"])
             limit: appends " LIMIT "+str(conditions["limit"])'''
-        
-        sql = f"SELECT {columns} FROM {self.table_name}"
-        try: sql += " WHERE "+str(conditions["where"])
+        sql_inp = f"SELECT {columns} FROM {self.table_name}"
+        try: sql_inp += " WHERE "+str(conditions["where"])
         except: pass
-        try: sql += " ORDER BY "+str(conditions["order"])
+        try: sql_inp += " ORDER BY "+str(conditions["order"])
         except: pass
-        try: sql += " LIMIT "+str(conditions["limit"])
+        try: sql_inp += " LIMIT "+str(conditions["limit"])
         except: pass
-        self.cursor.execute(sql)
-        if fetchone: return self.cursor.fetchone()
-        else: return self.cursor.fetchall()
-
-    def get_position(self, column_name, key_name, key_val) -> int:
-        if type(key_val) is str:
-            key_val = "\""+key_val+"\""
-        self.cursor.execute(f"""SELECT COUNT(*) FROM {self.table_name} WHERE {column_name} < (SELECT {column_name} FROM {self.table_name} WHERE {key_name} = {key_val}) ORDER BY {column_name}""")
-        return self.cursor.fetchone()+1
-db = db_class()
-print()
-db.add_rec("1", 2, "3", "4")
+        #print("sql_inp", sql_inp)
+        try:
+            self.cursor.execute(sql_inp)
+            return self.cursor.fetchall()
+        except:
+            print("get_rec error")
+            return ""
+    
+    '''def delete_table(self):
+        self.cursor.execute(f"DROP TABLE IF EXISTS {self.table_name}")'''
 
--- reaction_time_game/game.py
+++ reaction_time_game/game.py
@@ -1,87 +1,103 @@
 import pygame as pg
 from math import sqrt, log
 from random import randint
-from os import system, name
 from time import time as get_time
-import sql_db
-from sqlite3 import connect as sql_conn
+import db_code
+import mysql.connector
+
+'''
+Features to add:
+    Give the player's name a different colour in the leaderboard to make it stand out from everyone else
+    Make the leaderboard screen resize to fit the window size.
+    Add selectable difficulties, each with their own leaderboards.
+'''
 
 class window_class:
-    def __init__(self, width, height):
+    def __init__(self, width:int, height:int) -> None:
+        '''Hold information on the PyGame window.'''
         self.display, self.height, self.width = pg.display.set_mode((width, height)), height, width
 
 class target_class:
-    def __init__(self, pos, radius):
+    def __init__(self, pos, radius) -> None:
+        '''Stores targets, which are circles that appear in the PyGame window that dissapear when the cursor touches them.'''
         self.pos, self.radius = pos, radius
-    def draw(self, window, dist_to_mouse):
-        dist_relative_to_screen = dist_to_mouse/(sqrt(window.width**2 + window.height**2))
-        gradient = 0.5
-        
-        #r = max(min(int((gradient*255-dist_to_mouse)/gradient), 254), 0)
+    def draw(self, window, dist_to_cursor:float) -> None:
+        '''Draws the target to the PyGame window. The window still needs to be flipped (using pg.display.flip()) for this to appear.'''
+        dist_relative_to_screen = dist_to_cursor/(sqrt(window.width**2 + window.height**2))
+        gradient = 0.5 # You can adust this to change the distance at which the target becomes more red
         r = max(min(int(-log(dist_relative_to_screen*gradient)/7*255)+50, 255), 0)
         g = 50
-        #b = min(int(dist_to_mouse/gradient), 254)
         b = 255-r
-        #print("rgb",[r,g,b])
         pg.draw.circle(window.display, [r,g,b], (self.pos[0],self.pos[1]), self.radius, 0)
 
-class mouse_class:
-    def __init__(self, radius):
+class cursor_class:
+    def __init__(self, radius) -> None:
+        '''Holds information on the cursor.'''
         self.radius, self.pos = radius, pg.mouse.get_pos()
-    def update_pos(self):
+    def __update_pos(self) -> None:
+        '''Updates self.pos to be equal to the coordinates of the cursor.'''
         self.pos = pg.mouse.get_pos()
-    def draw(self, window):
-        self.update_pos()
+    def draw(self, window) -> None:
+        '''Draws a circle around the cursor.'''
+        self.__update_pos()
         pg.draw.circle(window.display, (130,150,25), (self.pos[0],self.pos[1]), self.radius, 0)
 
 class input_box_class:
     def __init__(self, x_pos, y_pos, width, height, font, text=""):
-        self.rect, self.colour = pg.Rect(x_pos, y_pos, width, height), pg.Color("lightskyblue3")
+        '''Class for a text input box.'''
+        self.rect, self.colour = pg.Rect(x_pos, y_pos, width, height), pg.Color("dodgerblue2")
         self.font, self.text = font, text
         self.txt_surface = self.font.render(self.text, True, self.colour)
-        self.active = False
-    def handle_event(self, event):
-        text_to_return = ""
+        self.active = True
+    def handle_event(self, event) -> str:
+        '''Checks to see if the user has selected/deselected the input box, and manages typing into the box.
+        'event' should be a PyGame event (ie. from pg.event.get()).'''
+        text_typed = ""
         if event.type == pg.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos): self.active = not self.active # Toggle whether the text box is active or not
             else: self.active = False
             self.colour = pg.Color("dodgerblue2") if self.active else pg.Color("lightskyblue3") # Change the current colour of the input box
         if event.type == pg.KEYDOWN and self.active:
-            if event.key == pg.K_RETURN: text_to_return, self.text = str(self.text), ""
+            if event.key == pg.K_RETURN: text_typed, self.text = str(self.text), ""
             elif event.key == pg.K_BACKSPACE: self.text = self.text[:-1]
             elif len(self.text) < 15: self.text += event.unicode
             self.txt_surface = self.font.render(self.text, True, self.colour) # Re-render the text
-        return text_to_return
-    def update(self): # Resize the box if the text is too long
+        return text_typed
+    def __update_width(self) -> None:
+        '''Resizes the box if the text is too long.'''
         self.rect.w = max(200, self.txt_surface.get_width()+10)
-    def draw(self, window):
+    def draw(self, window) -> None:
+        '''Draws the text imput box and the 'Enter your username' prompt to the PyGame window. The window still needs to be flipped (using pg.display.flip()) for this to appear.'''
+        self.__update_width()
         window.display.blit(self.font.render("Enter your username:", True, pg.Color(200,220,200)), (self.rect.x, self.rect.y-50)) # Blit the prompt text
         window.display.blit(self.txt_surface, (self.rect.x+5, self.rect.y+2)) # Blit the input box
         pg.draw.rect(window.display, self.colour, self.rect, 2) # Blit the rect
 
-def update_game_display(window, mouse, target_bank):
+def update_game_display(window, cursor: cursor_class, target_bank: list) -> None:
+    '''Updates all aspects of the window during the reaction game, including the mouse and targets. Also slips the window.'''
     window.display.fill((30, 30, 30))
     for i in range(len(target_bank)-1,-1,-1):
         target = target_bank[i]
-        dist_to_mouse = int(sqrt(((target.pos[0]-mouse.pos[0])**2) + ((target.pos[1]-mouse.pos[1])**2)))
-        if dist_to_mouse < target.radius+mouse.radius: target_bank.pop(i)
-        else: target.draw(window, dist_to_mouse)
-    mouse.draw(window)
+        dist_to_cursor = int(sqrt(((target.pos[0]-cursor.pos[0])**2) + ((target.pos[1]-cursor.pos[1])**2)))
+        if dist_to_cursor < target.radius+cursor.radius: target_bank.pop(i)
+        else: target.draw(window, dist_to_cursor)
+    cursor.draw(window)
     pg.display.flip() # Updates the window
 
-def display_paragraph(window, text_display_bank):
+def display_paragraph(window, text_display_bank:list) -> None:
+    '''Blits all lines of text in the text_display_bank to the window, then flips the window.
+    I intend to add some kind of atuo-text resizing so that it will always fit the screen later.'''
     window.display.fill((30, 30, 30))
-    font_size = 25
+    font_size, colour = 25, pg.Color(200, 220, 200)
     font = pg.font.SysFont("monospace", font_size)
-    colour = pg.Color(200, 220, 200)
-    x_pos = 20
-    y_pos = 20
+    x_pos, y_pos = 20, 20
     for line_pos in range(len(text_display_bank)):
         window.display.blit(font.render(text_display_bank[line_pos], True, colour), (x_pos, font_size*line_pos+y_pos))
     pg.display.flip()
 
-def main_game(window):
-    mouse = mouse_class(10)
+def main_game(window) -> float:
+    '''The main loop for the reaction time game. Returns the game_time, which is the amount of time that the game took as a float (rounded to 3 decimal places).'''
+    cursor = cursor_class(10)
     target_bank, targets = [], 10
     for i in range(targets):
         target_bank.append(target_class([randint(int(window.width*0.1),int(window.width*0.9)), randint(int(window.height*0.1),int(window.height*0.9))], 8))
@@ -93,15 +109,14 @@ def main_game(window):
                 running_game = False
                 pg.quit()
                 break
-        mouse.update_pos()
-        update_game_display(window, mouse, target_bank)
+        update_game_display(window, cursor, target_bank)
         if len(target_bank) == 0:
             game_time = round(get_time()-start_time, 3)
             running_game = False
     return game_time
 
-
-def main_input(window):
+def main_input(window) -> str:
+    '''The main loop for the input box, where the user's username is collected. Returns the username as a string.'''
     font = pg.font.SysFont("monospace", 32)
     input_box = input_box_class(100, 100, 140, 40, font)
     running_input = True
@@ -113,24 +128,34 @@ def main_input(window):
                 break
             username = input_box.handle_event(event)
             if username != "": return username
-        input_box.update()
         window.display.fill((30, 30, 30))
         input_box.draw(window)
         pg.display.flip()
 
-def main_db(window, game_time, username):
-    db, table_name = sql_db.db_class(sql_conn("reaction_time_game\\sql_db\\leaderboard.db")), "tblLeaderboard"
+def main_db(window, game_time:float, username:str) -> None:
+    '''The code managing updating the MySQL servers, then displaying the leaderboard.'''
+    conn = mysql.connector.connect(host="127.0.0.1", user="root", password="ytkxp2KMmXZU75mufMCP", database="leaderboard_db")
+    db = db_code.db_class(conn, "tbl_leaderboard")
     text_display_bank = ["You won in "+str(game_time)+" seconds"]
-    db.create_table(table_name)
-    rec_exists, user_data = db.get_rec(table_name, username)
-    if rec_exists:
+    db.create_table("userName VARCHAR(15)", "time FLOAT", "PRIMARY KEY (userName)")
+    user_data = db.get_rec(where=f"""userName = \"{username}\"""")
+    print("user_data", user_data)
+    if user_data != "" and user_data != []:
+        user_data = user_data[0]
         if game_time < user_data[1]:
             time_change = str(round(user_data[1]-game_time, 3))
             text_display_bank += ["", "Well Done! You beat your previous fastest", "time by "+time_change+" seconds."]
-            db.update_rec(table_name, "time", game_time, "userName", username)
+            db.update_rec("time", game_time, "userName", username)
         else: text_display_bank += ["", "Your fastest time", "is "+str(user_data[1])+" seconds. Keep trying!"]
-    else: db.add_rec(username, game_time, table_name)
-    text_display_bank += ["","Leaderboard:"]+db.get_leaderboard(table_name)+["","Your leaderboard position: "+str(db.get_position(table_name,"time","userName",username))] + ["Press Q to quit"]
+    else:
+        db.add_rec(username, game_time)
+        user_data = [username, game_time]
+    text_display_bank += ["", "Leaderboard:"]
+    leaderboard_bank = db.get_rec("*", order="time", limit=10)
+    for position in range(len(leaderboard_bank)):
+        text_display_bank += [str(position+1)+" - "+leaderboard_bank[position][0]+" with a time of "+str(leaderboard_bank[position][1])+" seconds"]
+    text_display_bank += ["","Your leaderboard position: "+str(db.get_rec("COUNT(*)", where="time <"+str(user_data[1]), order="time")[0][0])]
+    text_display_bank += ["Press Q to quit"]
     display_paragraph(window, text_display_bank)
     running_db = True
     while running_db:
