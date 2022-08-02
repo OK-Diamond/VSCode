69c70e08f3a2eef46eedfc7f1be7389d0a9e9027
OK_Diamond <ok.thekirks@gmail.com>
2022-08-02 15:56:09 +0100

    Main chess functionality done to a level that I am satisfied with, at least for now.

--- ChessMate/main.py
+++ ChessMate/main.py
@@ -1,14 +1,16 @@
-import pygame
-import os
+#import pygame
+import os # Used for the clear() function.
 
-def clear() -> None: os.system('cls' if os.name in ('nt', 'dos') else 'clear') # Clears the console
+def clear() -> None:
+    '''Clears the console'''
+    os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 class board_class:
-    def __init__(self, encoded_board="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") -> None:
+    def __init__(self, encoded_board="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0") -> None:
         '''Inialises the game board. 'encoded_board' parameter loads a string-encoded board and defaults to the starting board.'''
         self.board = []
-        boardstring, self.player_to_move, self.castle_options, self.en_passant, self.halfmove_clock, self.fullmove_count = encoded_board.split(" ")
-        self.halfmove_clock, self.fullmove_count = int(self.halfmove_clock), int(self.fullmove_count)
+        boardstring, self.player_to_move, self.castle_options, self.en_passant, self.halfmove_clock = encoded_board.split(" ")
+        self.halfmove_clock = int(self.halfmove_clock)
         for i in boardstring.split("/"):
             self.board.append([])
             for j in i:
@@ -21,6 +23,7 @@ class board_class:
         return
 
     def encode_to_str(self) -> str:
+        '''Translates the board into a single string, storing all neccessary information'''
         encoded_board = ""
         blank_count = 0
         for i in self.board:
@@ -37,10 +40,11 @@ class board_class:
                 blank_count = 0
             encoded_board += "/"
         encoded_board = encoded_board[0:-1]
-        encoded_board += " "+self.player_to_move+" "+self.castle_options+" "+self.en_passant+" "+str(self.halfmove_clock)+" "+str(self.fullmove_count)
+        encoded_board += " "+self.player_to_move+" "+self.castle_options+" "+self.en_passant+" "+str(self.halfmove_clock)
         return encoded_board
     
     def display_board(self) -> None:
+        '''Outputs the board to the console in a user-friendly format'''
         print("+—————————————————+")
         for y in range(8):
             print("| ", end="")
@@ -51,31 +55,31 @@ class board_class:
         return
 
     def get_possible_moves(self) -> list:
+        '''Returns a list of all possible boardstates after the current one. I had prevously stored the moves themselves (e.g. e3 to f2) but I found that that would have made it difficult to interfact with the database foring boardstates; after all, you can achieve the exact same boardstate via different moves.'''
         boardlist = []
         #print("len(self.board)", len(self.board))
         for i in range(len(self.board)):
             for j in range(len(self.board[i])):
                 if (self.player_to_move == "w" and self.board[i][j] in ["P", "B", "N", "R", "Q", "K"]) or (self.player_to_move == "b" and self.board[i][j] in ["p", "b", "n", "r", "q", "k"]):
-                    possible_boards = self.move_test([i, j])
+                    possible_boards = self._move_test([i, j])
                     if len(possible_boards) > 0:
                         for k in possible_boards:
                             boardlist.append(k)
         return boardlist
 
-    def move_test(self, pos: list) -> list:
+    def _move_test(self, pos: list) -> list:
+        '''Private function called by 'get_possible_moves' to find all possible moves from a certain piece.'''
         boardlist = []
         piece_type, old_en_passant = self.board[pos[0]][pos[1]], self.en_passant
         template = board_class(self.encode_to_str())
+        template.halfmove_clock += 1
         template.en_passant = "-"
-        if self.player_to_move == "w":
-            template.fullmove_count += 1
-            template.player_to_move = "b"
-        else:
-            template.player_to_move = "w"
+        template.player_to_move = "b" if self.player_to_move == "w" else "w"
+        
         opponent_piece_bank = ["P", "B", "N", "R", "Q", "K"] if piece_type == str.lower(piece_type) else ["p", "b", "n", "r", "q", "k"]
         # Pawn
         if piece_type in ["P", "p"]:
-            template.fullmove_count = 0
+            template.halfmove_clock = 0
             if piece_type == "P": direction = -1 # White
             else: direction = 1 # Black
             for diagonal in [-1, 1]: # Check for opposing pieces on diagonals and en passant
@@ -114,7 +118,7 @@ class board_class:
                         temp_board = board_class(template.encode_to_str())
                         temp_board.board[pos[0]][pos[1]], temp_board.board[pos[0]+y][pos[1]+x] = " ", piece_type
                         if self.board[pos[0]+y][pos[1]+x] in opponent_piece_bank:
-                            temp_board.fullmove_count = 0
+                            temp_board.halfmove_clock = 0
                         boardlist.append(temp_board.encode_to_str())
                         del temp_board
         # Bishop and Diagonal Queen
@@ -127,7 +131,7 @@ class board_class:
                             temp_board = board_class(template.encode_to_str())
                             temp_board.board[pos[0]][pos[1]], temp_board.board[pos[0]+(target*y)][pos[1]+(target*x)] = " ", piece_type
                             if self.board[pos[0]+(target*y)][pos[1]+(target*x)] in opponent_piece_bank:
-                                temp_board.fullmove_count = 0
+                                temp_board.halfmove_clock = 0
                                 piece_found = True
                             boardlist.append(temp_board.encode_to_str())
                             del temp_board
@@ -143,11 +147,10 @@ class board_class:
                             temp_board = board_class(template.encode_to_str())
                             temp_board.board[pos[0]][pos[1]], temp_board.board[pos[0]+(target*y)][pos[1]+(target*x)] = " ", piece_type
                             if self.board[pos[0]+(target*y)][pos[1]+(target*x)] in opponent_piece_bank:
-                                temp_board.fullmove_count = 0
+                                temp_board.halfmove_clock = 0
+                                piece_found = True
                             boardlist.append(temp_board.encode_to_str())
                             del temp_board
-                            if self.board[pos[0]+(target*y)][pos[1]+(target*y)] in opponent_piece_bank:
-                                piece_found = True
                         else:
                             piece_found = True
         # King
@@ -159,18 +162,16 @@ class board_class:
                             temp_board = board_class(template.encode_to_str())
                             temp_board.board[pos[0]][pos[1]], temp_board.board[pos[0]+y][pos[1]+x] = " ", piece_type
                             if self.board[pos[0]+y][pos[1]+x] in opponent_piece_bank:
-                                temp_board.fullmove_count = 0
+                                temp_board.halfmove_clock = 0
                             boardlist.append(temp_board.encode_to_str())
                             del temp_board
         return boardlist
-        
-        
 
-def process_turn(gameboard:board_class):
+def process_turn(gameboard:board_class) -> str:
+    '''Manages a player's turn, which involve displaying their move options and handling inputs.'''
     error_msg = ""
     while True:
         clear()
-        print(error_msg)
         if gameboard.player_to_move == "w":
             print("White's Turn")
         else:
@@ -183,20 +184,25 @@ def process_turn(gameboard:board_class):
             a = board_class(move_bank[i])
             print(f"{i+1}. {move_bank[i]}")
             a.display_board()
+        print("halfmove_clock", gameboard.halfmove_clock)
+        print("\n", error_msg)
         user_inp = input("Please enter the number of the move that you would like to make: ")
         try:
             user_inp = int(user_inp)
             if user_inp in range(1, len(move_bank)+1):
                 return move_bank[user_inp-1]
             else:
-                error_msg = f"Error: User input must be in the range 1-{len(move_bank)+1}"
+                error_msg = f"Input Error: User input must be in the range 1-{len(move_bank)+1}"
         except:
-            error_msg = "Error: Input must be an integer"
+            error_msg = "Input Error: Input must be an integer"
 
-def check_for_game_over(gameboard:board_class) -> str:
-    if gameboard.fullmove_count >= 50:
-        return "Draw"
+def check_for_game_over(gameboard:board_class, previous_moves_bank:list) -> tuple[str, str]:
+    '''Checks various aspects of the board to see if the game has ended. Returns two strings: The first is the victory type (W, B, D or blank) and the second is some text that can be displayed to the user to explain why the game has ended.'''
+    if gameboard.halfmove_clock >= 50:
+        return "d", "Draw by the 50-move rule"
+    
     white_king_found, black_king_found = False, False
+    
     for i in gameboard.board or len(gameboard.get_possible_moves() == 0):
         for j in i:
             if j == "K":
@@ -204,17 +210,37 @@ def check_for_game_over(gameboard:board_class) -> str:
             elif j == "k":
                 black_king_found = True
     if not white_king_found:
-        return "Black Wins"
+        return "b", "Black wins by checkmate"
     elif not black_king_found:
-        return "White Wins"
-    
+        return "w", "White wins by checkmate"
+    threefold_repetition_counter = 0
+    for i in previous_moves_bank:
+        for j in i:
+            if gameboard.encode_to_str().split(" ")[0:-1] == str(j).split(" ")[0:-1]: # The [0:-1] is used to remove the halfmove clock, which is not tracked for a threefold repetition draw.
+                threefold_repetition_counter += 1
+                print(i, j)
+            if threefold_repetition_counter >= 5: # Requires 5 matches (instead of the expected three) because it counts each other board twice + it counts itself once.
+                print(gameboard.encode_to_str())
+                return "D", "Draw by threefold repetition"
+    return "", ""
 
-def main():
+def main() -> None:
+    previous_moves_bank = []
     gameboard = board_class()
     game_running = True
     while game_running:
+        board1 = gameboard.encode_to_str()
         gameboard = board_class(process_turn(gameboard))
-        check_for_game_over(gameboard)
+        board2 = gameboard.encode_to_str()
+        previous_moves_bank.append([board1, board2])
+        # Manage the halfmove clock
+        print("previous_moves_bank", previous_moves_bank, "\n\n")
+        
+        game_state, result = check_for_game_over(gameboard, previous_moves_bank)
+        if result != "":
+            game_running = False
+    #print(previous_moves_bank)
+    print(game_state, result)
 
 if __name__ == "__main__":
     main()
